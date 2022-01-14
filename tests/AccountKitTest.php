0587d8c6b49eef3e381d37102097e5fa172a6300
Martin Zeitler <martin@syslogic.io>
2022-01-14 18:02:55 +0100

    excess spaces removed.

--- tests/AccountKitTest.php
+++ tests/AccountKitTest.php
@@ -15,8 +15,8 @@ class AccountKitTest extends TestCase {
 
     /** This method is called before the first test of this test class is run. */
     public static function setUpBeforeClass(): void {
-        self::assertTrue( getenv('HUAWEI_CLIENT_ID')     != false, 'Variable HUAWEI_CLIENT_ID  is not set.' );
-        self::assertTrue( getenv('HUAWEI_CLIENT_SECRET') != false, 'Variable HUAWEI_CLIENT_SECRET  is not set.' );
+        self::assertTrue( getenv('HUAWEI_CLIENT_ID')     != false, 'Variable HUAWEI_CLIENT_ID is not set.' );
+        self::assertTrue( getenv('HUAWEI_CLIENT_SECRET') != false, 'Variable HUAWEI_CLIENT_SECRET is not set.' );
         self::$client = new AccountKit( [
             'client_id'     => (int)    getenv('HUAWEI_CLIENT_ID'),
             'client_secret' => (string) getenv('HUAWEI_CLIENT_SECRET')
