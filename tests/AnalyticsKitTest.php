6b3188f3b60341c3e8213e2b7fea04b6c6a44378
Martin Zeitler <martin@syslogic.io>
2022-01-16 18:39:59 +0100

    tests extended.

--- tests/AnalyticsKitTest.php
+++ tests/AnalyticsKitTest.php
@@ -2,6 +2,8 @@
 namespace Tests;
 
 use HMS\AnalyticsKit\AnalyticsKit;
+use HMS\AnalyticsKit\ResultCodes;
+use stdClass;
 
 /**
  * HMS AnalyticsKit Test
@@ -19,8 +21,61 @@ class AnalyticsKitTest extends BaseTestCase {
         self::assertTrue( self::$client->is_ready(), self::CLIENT_NOT_READY );
     }
 
-    /** Test: Dummy. */
-    public function test_dummy() {
-        self::assertTrue( true );
+    /** Test: Exporting Personal Data. */
+    public function test_user_data_export() {
+        $aaid = 'test';
+        $result = self::$client->request_user_data_export( $aaid );
+        self::assertTrue($result instanceof stdClass );
+        self::assertObjectHasAttribute('result_code', $result);
+        self::assertTrue( $result->result_code === ResultCodes::REQUEST_SUCCESSFUL, "Error $result->result_code: $result->result_msg" );
     }
+
+    /** Test: Querying the Export Task Status. */
+    public function test_user_data_export_status() {
+        $aaid = 'test';
+        $result = self::$client->request_user_data_export_status( $aaid );
+        self::assertTrue($result instanceof stdClass );
+        self::assertObjectHasAttribute('result_code', $result);
+        self::assertTrue( $result->result_code === ResultCodes::REQUEST_SUCCESSFUL, "Error $result->result_code: $result->result_msg" );
+    }
+
+    /** Test: Deleting Personal Data. */
+    public function test_user_data_deletion() {
+        $aaid = 'test';
+        $result = self::$client->request_user_data_deletion( $aaid );
+        self::assertTrue($result instanceof stdClass );
+        self::assertObjectHasAttribute('result_code', $result);
+        self::assertTrue( $result->result_code === ResultCodes::REQUEST_SUCCESSFUL, "Error $result->result_code: $result->result_msg" );
+    }
+
+    /** Test: Querying the Deletion Task Status. */
+    public function test_user_data_deletion_status() {
+        $aaid = 'test';
+        $result = self::$client->request_user_data_deletion_status( $aaid );
+        self::assertTrue($result instanceof stdClass );
+        self::assertObjectHasAttribute('result_code', $result);
+        self::assertTrue( $result->result_code === ResultCodes::REQUEST_SUCCESSFUL, "Error $result->result_code: $result->result_msg" );
+    }
+
+    /** Test: Creating a Data Export Task. */
+
+    /** Test: Receiving the Execution Status of a Data Export Task. */
+
+    /** Test: Importing Custom User Attributes. */
+
+    /** Test: Importing Content. */
+
+    /** Test: Reporting User Behavior. */
+
+    /** Test: Querying Open Metrics and Dimensions. */
+    public function test_query_metrics_and_dimensions() {
+        $result = self::$client->query_metrics_and_dimensions( 'en', 10,  1 );
+        self::assertTrue($result instanceof stdClass );
+        self::assertObjectHasAttribute('result_code', $result);
+        self::assertTrue( $result->result_code === ResultCodes::REQUEST_SUCCESSFUL, "Error $result->result_code: $result->result_msg" );
+    }
+
+    /** Test: Querying Dimension Values. */
+
+    /** Test: Querying Statistical Metrics. */
 }
--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -12,41 +12,43 @@ use PHPUnit\Framework\TestCase;
  */
 abstract class BaseTestCase extends TestCase {
 
-    protected static int $project_id = 0;
     protected static string|null $package_name = null;
-    protected static string|null $api_key = null;
-
-    protected static int $client_id = 0;
-    protected static int $app_id = 0;
-
     protected static string|null $client_secret = null;
     protected static string|null $app_secret = null;
+    protected static string|null $api_key = null;
+    protected static int $project_id = 0;
+    protected static int $product_id = 0;
+    protected static int $client_id = 0;
+    protected static int $app_id = 0;
+    protected static int $cp_id = 0;
 
+    private const ENV_VAR_APP_SECRET = 'Variable HUAWEI_APP_SECRET is not set.';
+    private const ENV_VAR_APPLICATION_CREDENTIALS = 'Variable HUAWEI_APPLICATION_CREDENTIALS is not set.';
+    private const ENV_VAR_UPSTREAM_HMAC_VERIFICATION_KEY = 'Variable HUAWEI_UPSTREAM_HMAC_VERIFICATION_KEY is not set.';
     protected const CLIENT_NOT_READY = 'The client is not ready.';
-    protected const ENV_VAR_APPLICATION_CREDENTIALS = 'Variable HUAWEI_APPLICATION_CREDENTIALS is not set.';
-    protected const ENV_VAR_UPSTREAM_HMAC_VERIFICATION_KEY = 'Variable HUAWEI_UPSTREAM_HMAC_VERIFICATION_KEY is not set.';
 
     #[ArrayShape(['client_id' => "int", 'client_secret' => "null|string"])]
     protected static function get_secret(): array {
-        return [
-            'client_id'     => self::$app_id,
-            'client_secret' => self::$app_secret
-        ];
+        return ['client_id' => self::$app_id, 'client_secret' => self::$app_secret];
     }
 
     /** This method is called before the first test of this test class is run. */
     public static function setUpBeforeClass(): void {
+        self::assertTrue(getenv('HUAWEI_APP_SECRET') != false, self::ENV_VAR_APP_SECRET);
         self::assertTrue(getenv('HUAWEI_APPLICATION_CREDENTIALS') != false, self::ENV_VAR_APPLICATION_CREDENTIALS);
         self::assertTrue( getenv('HUAWEI_UPSTREAM_HMAC_VERIFICATION_KEY')  != false, self::ENV_VAR_UPSTREAM_HMAC_VERIFICATION_KEY);
+        $app_secret = getenv('HUAWEI_APP_SECRET'); // not contained in the JSON.
         $config_file = getenv('HUAWEI_APPLICATION_CREDENTIALS');
         if ( file_exists( $config_file ) ) {
             $config = json_decode(file_get_contents( $config_file ));
             if ( is_object( $config )) {
                 if ( property_exists( $config, 'client' ) && is_object( $config->client )) {
+                    self::$cp_id        =     (int) $config->client->cp_id;
                     self::$app_id        =    (int) $config->client->app_id;
-                    self::$app_secret    = (string) getenv('HUAWEI_APP_SECRET'); // not contained in the JSON.
+                    self::$app_secret    = (string) $app_secret;
                     self::$package_name  = (string) $config->client->package_name;
                     self::$project_id    =    (int) $config->client->project_id;
+                    self::$product_id    =    (int) $config->client->product_id;
                     self::$client_id     =    (int) $config->client->client_id;
                     self::$client_secret = (string) $config->client->client_secret;
                     self::$api_key       = (string) $config->client->api_key;
