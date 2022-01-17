72a2cfd2cac400bee81e3e36085d79409bfac9ff
Martin Zeitler <martin@syslogic.io>
2022-01-17 02:29:28 +0100

    AccountKit should handle the token refresh instead.

--- src/AccountKit/AccountKit.php
+++ src/AccountKit/AccountKit.php
@@ -11,7 +11,19 @@ use HMS\Core\Wrapper;
  */
 class AccountKit extends Wrapper {
 
+    /** oAuth2 Token related. */
+    private string|null $url_token_refresh = Constants::URL_OAUTH2_TOKEN_REFRESH_V3;
+    protected string|null $access_token = null;
+    protected string|null $refresh_token = null;
+    private int $token_expiry = 0;
+
+    /** ID Token related. */
+    private string|null $id_token = null;
+    private string|null $token_scope = null;
+    private string|null $union_id = null;
+    private string|null $open_id = null;
+
     public function __construct( array|string $config ) {
-        parent::__construct( $config, 3 );
+        parent::__construct( $config );
     }
 }
