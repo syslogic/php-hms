071fa0c9b3f1ef10667cb1214930ef6d12f1a40d
Martin Zeitler <martin@syslogic.io>
2022-01-17 19:53:14 +0100

    SearchKit: client_ip & request_id HTTP headers added.

--- src/SearchKit/SearchKit.php
+++ src/SearchKit/SearchKit.php
@@ -11,7 +11,26 @@ use HMS\Core\Wrapper;
  */
 class SearchKit extends Wrapper {
 
+    /**
+     * @var string|null $client_ip IP address of a user device.
+     * You are advised to pass it for localization when connecting to the Search Kit server.
+     */
+    private string|null $client_ip = null;
+
+    /** @var string|null $request_id .Unique ID of a request. */
+    private string|null $request_id = null;
+
     public function __construct( array|string $config ) {
         parent::__construct( $config );
+    }
+
+    /** Provide HTTP request headers as array. */
+    protected function auth_header(): array {
+        return [
+            "Content-Type: application/json", "Authorization: Bearer $this->access_token",
+            "X-Kit-AppID: $this->app_id",
+            "X-Kit-ClientIP: $this->client_ip",
+            "X-Kit-RequestID: $this->request_id"
+        ];
     }
 }
