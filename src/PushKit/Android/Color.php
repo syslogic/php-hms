15b207ea33b2d3bf2e8e6fe17d11464ff185ae90
Martin Zeitler <martin@syslogic.io>
2022-01-25 10:50:03 +0100

    few dummy implementations added.

--- src/PushKit/Android/Color.php
+++ src/PushKit/Android/Color.php
@@ -9,6 +9,39 @@ use HMS\Core\Model;
  *
  * @author Martin Zeitler
  */
-class Color {
+class Color extends Model {
 
+
+    protected array $mandatory_fields = [''];
+    protected array $optional_fields  = [''];
+
+    public function __construct( array $data ) {
+        $this->parse_array( $data );
+    }
+
+    private function parse_array( array $data ): void {
+        foreach ($data as $key => $value) {
+            if ( in_array($key, array_merge($this->mandatory_fields, $this->optional_fields)) ) {
+                $this->$key = $value;
+            }
+        }
+    }
+
+    static function fromArray( array $model ): Button {
+        return new Button( $model );
+    }
+
+    /** Conditionally adding array items. */
+    public function asArray(): array {
+        $data = [];
+        return $data;
+    }
+
+    public function asObject(): object {
+        return (object) $this->asArray();
+    }
+
+    function validate(): bool {
+        return true;
+    }
 }
--- src/PushKit/Android/LightSettings.php
+++ src/PushKit/Android/LightSettings.php
@@ -8,6 +8,38 @@ use HMS\Core\Model;
  *
  * @author Martin Zeitler
  */
-class LightSettings {
+class LightSettings extends Model {
 
+    protected array $mandatory_fields = [''];
+    protected array $optional_fields  = [''];
+
+    public function __construct( array $data ) {
+        $this->parse_array( $data );
+    }
+
+    private function parse_array( array $data ): void {
+        foreach ($data as $key => $value) {
+            if ( in_array($key, array_merge($this->mandatory_fields, $this->optional_fields)) ) {
+                $this->$key = $value;
+            }
+        }
+    }
+
+    static function fromArray( array $model ): Button {
+        return new Button( $model );
+    }
+
+    /** Conditionally adding array items. */
+    public function asArray(): array {
+        $data = [];
+        return $data;
+    }
+
+    public function asObject(): object {
+        return (object) $this->asArray();
+    }
+
+    function validate(): bool {
+        return true;
+    }
 }
