d0d040795f3a0aa994e67424384a87c89d933966
Martin Zeitler <martin@syslogic.io>
2022-01-25 14:46:55 +0100

    example updated.

--- example/account_kit_refresh_token.php
+++ example/account_kit_refresh_token.php
@@ -1,9 +1,19 @@
-<?php
-use HMS\AccountKit\AccountKit;
+<html>
+    <head>
+    </head>
+    <body>
+    <?php
+    /**
+     * AccountKit Example
+     *
+     * @author Martin Zeitler
+     */
+    use HMS\AccountKit\AccountKit;
+    $config = ['app_id' => getenv('HUAWEI_APP_ID'), 'app_secret' => getenv('HUAWEI_APP_SECRET')];
+    $api = new AccountKit( $config );
+    if ( $api->is_ready() ) {
 
-/**
- * AccountKit Example
- * @author Martin Zeitler
- */
-?>
-
+    }
+    ?>
+    </body>
+</html>
