7a989ffba6698cbe09d957f2854e230675cb5155
Martin Zeitler <martin@syslogic.io>
2022-01-25 10:34:34 +0100

    urls removed.

--- example/account_kit_refresh_token.php
+++ example/account_kit_refresh_token.php
@@ -3,8 +3,6 @@ use HMS\AccountKit\AccountKit;
 
 /**
  * AccountKit Example
- *
- * @see <a href="https://developer.huawei.com/consumer/en/doc/development/HMSCore-References/account-obtain-token_hms_reference-0000001050048618">AccountKit</a>
  * @author Martin Zeitler
  */
 ?>
