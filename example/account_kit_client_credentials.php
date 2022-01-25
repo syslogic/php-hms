79a232263fada344843a13c1694ca60fba02a298
Martin Zeitler <martin@syslogic.io>
2022-01-25 10:33:17 +0100

    few dummy examples added.

---
+++ example/account_kit_client_credentials.php
@@ -0,0 +1,11 @@
+<?php
+use HMS\AccountKit\AccountKit;
+
+/**
+ * AccountKit Example
+ *
+ * @see <a href="https://developer.huawei.com/consumer/en/doc/development/HMSCore-References/account-obtain-token_hms_reference-0000001050048618">AccountKit</a>
+ * @author Martin Zeitler
+ */
+?>
+
