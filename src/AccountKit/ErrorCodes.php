f12e06a51e5d6de3495ee541b06f0b4941d913a4
Martin Zeitler <martin@syslogic.io>
2022-01-29 00:07:05 +0100

    HMS\AccountKit\ErrorCodes

--- src/AccountKit/ErrorCodes.php
+++ src/AccountKit/ErrorCodes.php
@@ -9,4 +9,18 @@ namespace HMS\AccountKit;
  */
 class ErrorCodes {
 
+    /** 1101 / 20002 - The value of `client_id` is in an incorrect format. */
+    public const CLIENT_ID_HAS_INVALID_FORMAT     = 20002;
+
+    /** 1101 / 20022 - The value of `redirect_uri` is in an incorrect format. */
+    public const REDIRECT_URI_HAS_INVALID_FORMAT  = 20022;
+
+    /** 1101 / 20023 - The value of `redirect_uri` in `code` does not match the redirection URL set in AppGallery Connect. */
+    public const REDIRECT_URI_DOES_NOT_MATCH_CODE = 20023;
+
+    /** 1101 / 20086 - Invalid need_code parameter. */
+    public const INVALID_NEED_CODE_PARAMETER      = 20086;
+
+
+    // TODO ...
 }
