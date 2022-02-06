b6ea3230accc495957585f79b51be7d08c404f24
Martin Zeitler <martin@syslogic.io>
2022-02-06 19:37:45 +0100

    todo comment added.

--- src/LocationKit/ResultCodes.php
+++ src/LocationKit/ResultCodes.php
@@ -2,10 +2,14 @@
 namespace HMS\LocationKit;
 
 /**
- * Class HMS LocationKit Result Codes
+ * TODO: Class HMS LocationKit Result Codes
  *
+ * @see <a href="https://developer.huawei.com/consumer/en/doc/development/HMSCore-References/web-error-code-0000001052202593">Result Codes</a>
  * @author Martin Zeitler
  */
 class ResultCodes {
+
+    /** 0 - Request successful. */
+    public const OK = 0;
 
 }
