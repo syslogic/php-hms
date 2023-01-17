5c08a0cae98cbc4d4aa8b73416dccb2f824941e4
Martin Zeitler <martin@syslogic.io>
2023-01-17 21:42:18 +0100

    DriveKit example updated.

--- example/drivekit.php
+++ example/drivekit.php
@@ -1,21 +1,17 @@
 <?php
+require_once '../vendor/autoload.php';
 
 use HMS\AccountKit\AccountKit;
 use HMS\DriveKit\DriveKit;
-error_reporting(-1);
+// error_reporting(-1);
+
 $token_path = '../../.credentials/huawei_token.json';
-if (! is_writeable($token_path)) {
-    die( 'unable to cache token: ' . $token_path );
-}
+if (! is_writeable($token_path)) {die( 'unable to cache the token: '.$token_path );}
+if (! isset($_SERVER['HUAWEI_OAUTH2_CLIENT_ID'])) {die( 'missing variable: HUAWEI_OAUTH2_CLIENT_ID' );}
+if (! isset($_SERVER['HUAWEI_OAUTH2_CLIENT_SECRET'])) {die( 'missing variable: HUAWEI_OAUTH2_CLIENT_SECRET' );}
+if (! isset($_SERVER['HUAWEI_OAUTH2_REDIRECT_URL'])) {die( 'missing variable: HUAWEI_OAUTH2_REDIRECT_URL' );}
+if (! isset($_SERVER['HUAWEI_OAUTH2_API_SCOPE'])) {die( 'missing variable: HUAWEI_OAUTH2_API_SCOPE' );}
 
-if (! isset($_SERVER['HUAWEI_OAUTH2_REDIRECT_URL'])) {
-    die( 'missing variable: HUAWEI_OAUTH2_REDIRECT_URL' );
-}
-if (! isset($_SERVER['HUAWEI_OAUTH2_API_SCOPE'])) {
-    die( 'missing variable: HUAWEI_OAUTH2_API_SCOPE' );
-}
-
-/*
 $api = new AccountKit( [
     'app_id' => $_SERVER['HUAWEI_OAUTH2_CLIENT_ID'],
     'app_secret' => $_SERVER['HUAWEI_OAUTH2_CLIENT_SECRET'],
@@ -24,25 +20,19 @@ $api = new AccountKit( [
     'debug' => true
 ]);
 
-
-
-
 // https://developer.huawei.com/consumer/en/doc/development/HMSCore-Guides/web-get-access-token-0000001050048946#section151118514311
 if (isset($_GET['code'])) {
     $access_token = $api->get_access_token_by_auth_code( $_GET['code'] );
     if ($access_token != null) {
-        // file_put_contents($token_path, $access_token);
-        $api2 = new DriveKit( ['access_token' => $access_token] );
-        $result = $api2->getAbout()->get();
-        // die('<pre>' . print_r($result, true) . '</pre>');
+        file_put_contents($token_path, $access_token);
+        $drive = new DriveKit( ['access_token' => $access_token] );
+        $result = $drive->getAbout()->get();
+        die('<pre>' . print_r($result, true) . '</pre>');
     }
 } else if (file_exists($token_path) && filesize($token_path) > 2) {
     // load previously authorized token from a file, if it exists.
     $access_token = json_decode(file_get_contents($token_path), true);
 }
-$oauth2_url = $api->get_login_url();
-*/
-
 ?>
 <html lang="en">
     <head>
@@ -50,12 +40,7 @@ $oauth2_url = $api->get_login_url();
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <script type="text/javascript">
             function redirect() {
-                let url = 'https://oauth-login.cloud.huawei.com/oauth2/v3/authorize';
-                url += '?response_type=code&access_type=offline&state=state_parameter_passthrough_value';
-                url += '&client_id=<?=$_SERVER['HUAWEI_OAUTH2_CLIENT_ID'];?>';
-                url += '&redirect_uri=<?=/*$_SERVER['HUAWEI_OAUTH2_REDIRECT_URL'] */'';?>';
-                url += '&scope=<?= /*$_SERVER['HUAWEI_OAUTH2_API_SCOPE']; */''; ?>';
-                window.location.href=url;
+                window.location.href = '<?= $api->get_login_url(); ?>';
             }
         </script>
     </head>
