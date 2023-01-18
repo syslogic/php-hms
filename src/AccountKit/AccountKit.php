22ecf3d134ce0c142fd9e95560d29b4ffdaeb943
Martin Zeitler <martin@syslogic.io>
2023-01-18 21:20:46 +0100

    token revocation method added.

--- src/AccountKit/AccountKit.php
+++ src/AccountKit/AccountKit.php
@@ -12,22 +12,6 @@ use stdClass;
  */
 class AccountKit extends Wrapper {
 
-    /** oAuth2 Token related. */
-    private string $url_token = Constants::URL_OAUTH2_TOKEN;
-    private string $url_token_info = Constants::ACCOUNT_KIT_TOKEN_INFO;
-    private string $url_user_info  = Constants::ACCOUNT_KIT_USER_INFO;
-
-    /** value `client_credentials` isn't documented, but it's working. */
-    private array $grant_types = [
-        'authorization_code', 'client_credentials', 'refresh_token'
-    ];
-
-    /** ID Token related. */
-    private string|null $id_token = null;
-    private string|null $token_scope = null;
-    private string|null $union_id = null;
-    private string|null $open_id = null;
-
     public function __construct( array $config ) {
         parent::__construct( $config );
         $this->post_init();
@@ -46,14 +30,13 @@ class AccountKit extends Wrapper {
      * @see <a href="https://developer.huawei.com/consumer/en/doc/development/HMSCore-References/account-obtain-token_hms_reference-0000001050048618">Obtaining Access Token</a>
      */
     public function get_access_token(): string|null {
-        $result = $this->guzzle_post($this->url_token, [
+        $result = $this->guzzle_post(Constants::URL_OAUTH2_TOKEN, [
             'Content-Type' => 'application/x-www-form-urlencoded; charset=utf-8'
         ], [
             'grant_type'    => 'client_credentials',
             'client_id'     => $this->oauth2_client_id,
             'client_secret' => $this->oauth2_client_secret
         ], true);
-
         return $this->parse_result($result);
     }
 
@@ -63,7 +46,7 @@ class AccountKit extends Wrapper {
      * @return stdClass|null the whole retrieved token object.
      */
     public function get_access_token_by_auth_code( string $authorization_code ): stdClass|bool {
-        $result = $this->guzzle_post($this->url_token, [
+        $result = $this->guzzle_post(Constants::URL_OAUTH2_TOKEN, [
             'Content-Type' => 'application/x-www-form-urlencoded; charset=utf-8'
         ], [
             'grant_type'    => 'authorization_code',
@@ -73,7 +56,7 @@ class AccountKit extends Wrapper {
             'code'          => $authorization_code
         ], true);
 
-        /* provide an absolute value as the expiry timestamp. */
+        /* provide an absolute value as token expiry timestamp. */
         if (property_exists($result, 'expires_in')) {
             $result->token_expiry = time() + $result->expires_in;
             unset($result->expires_in);
@@ -87,7 +70,7 @@ class AccountKit extends Wrapper {
      * @return stdClass|null the whole retrieved token object.
      */
     public function get_access_token_by_refresh_token( string $refresh_token ): stdClass|bool {
-        $result = $this->guzzle_post($this->url_token, [
+        $result = $this->guzzle_post(Constants::URL_OAUTH2_TOKEN, [
             'Content-Type' => 'application/x-www-form-urlencoded; charset=utf-8'
         ], [
             'grant_type'    => 'refresh_token',
@@ -96,12 +79,24 @@ class AccountKit extends Wrapper {
             'code'          => $refresh_token
         ], true);
 
-        /* provide an absolute value as the expiry timestamp. */
+        /* provide an absolute value as token expiry timestamp. */
         if (property_exists($result, 'expires_in')) {
             $result->token_expiry = time() + $result->expires_in;
             unset($result->expires_in);
         }
         return $result;
+    }
+
+    /**
+     * Revoke an access or refresh token.
+     * @return stdClass|bool an empty response.
+     */
+    public function revoke_token( string $token ): stdClass|bool {
+        return $this->guzzle_post(Constants::URL_OAUTH2_TOKEN_REVOCATION, [
+            'Content-Type' => 'application/x-www-form-urlencoded; charset=utf-8'
+        ], [
+            'token' => $token
+        ], true);
     }
 
     /**
@@ -114,7 +109,7 @@ class AccountKit extends Wrapper {
      * @see <a href="https://developer.huawei.com/consumer/en/doc/development/HMSCore-References/account-gettokeninfo-0000001050050585">Parsing an Access Token</a>
      */
     public function parse_access_token( string|null $access_token ): TokenInfo|null {
-        $result = $this->guzzle_post(Constants::ACCOUNT_KIT_TOKEN_INFO, [
+        $result = $this->guzzle_post(Constants::URL_ACCOUNT_KIT_TOKEN_INFO, [
             'Content-Type' => 'application/x-www-form-urlencoded;charset=utf-8',
             'Authorization' => 'Bearer '.$this->access_token
         ], [
@@ -139,7 +134,7 @@ class AccountKit extends Wrapper {
      * @see <a href="https://developer.huawei.com/consumer/en/doc/development/HMSCore-References/get-user-info-0000001060261938">Obtaining User Information</a>
      */
     public function get_user_info( string|null $user_access_token ): UserInfo|stdClass {
-        $result = $this->guzzle_post($this->url_user_info, [
+        $result = $this->guzzle_post(Constants::URL_ACCOUNT_KIT_USER_INFO, [
             'Content-Type: application/x-www-form-urlencoded;charset=utf-8',
             "Authorization: Bearer $this->access_token" // OK
         ], [
@@ -159,14 +154,14 @@ class AccountKit extends Wrapper {
     }
 
     /**
-     * TODO: Verify an ID Token URL_OAUTH2_TOKEN_INFO.
+     * TODO: Verify an ID Token.
      *
      * @param string|null $id_token
      * @return IdTokenInfo|null
      * @see <a href="https://developer.huawei.com/consumer/en/doc/development/HMSCore-References/account-verify-id-token_hms_reference-0000001050050577">Verifying an ID Token</a>
      */
     public function verify_id_token( string|null $id_token ): IdTokenInfo|stdClass {
-        $result = $this->guzzle_post($this->url_token_info, [
+        $result = $this->guzzle_post(Constants::URL_OAUTH2_TOKEN_INFO, [
             'Content-Type: application/x-www-form-urlencoded;charset=utf-8',
             "Authorization: Bearer $this->access_token" // OK
         ], [
--- src/AccountKit/Constants.php
+++ src/AccountKit/Constants.php
@@ -7,9 +7,9 @@ namespace HMS\AccountKit;
  * @author Martin Zeitler
  */
 class Constants {
-
     public const URL_OAUTH2_TOKEN       = "https://oauth-login.cloud.huawei.com/oauth2/v3/token";
     public const URL_OAUTH2_TOKEN_INFO  = "https://oauth-login.cloud.huawei.com/oauth2/v3/tokeninfo";
-    public const ACCOUNT_KIT_TOKEN_INFO = "https://oauth-api.cloud.huawei.com/rest.php?nsp_fmt=JSON&nsp_svc=huawei.oauth2.user.getTokenInfo";
-    public const ACCOUNT_KIT_USER_INFO  = "https://account.cloud.huawei.com/rest.php?nsp_svc=GOpen.User.getInfo";
+    public const URL_OAUTH2_TOKEN_REVOCATION = "https://oauth-login.cloud.huawei.com/oauth2/v3/revoke";
+    public const URL_ACCOUNT_KIT_TOKEN_INFO = "https://oauth-api.cloud.huawei.com/rest.php?nsp_fmt=JSON&nsp_svc=huawei.oauth2.user.getTokenInfo";
+    public const URL_ACCOUNT_KIT_USER_INFO  = "https://account.cloud.huawei.com/rest.php?nsp_svc=GOpen.User.getInfo";
 }
