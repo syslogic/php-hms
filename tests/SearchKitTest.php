c00558cccaa72524f9ca7d533a15eb0fccb466d1
Martin Zeitler <martin@syslogic.io>
2023-02-15 14:31:12 +0100

    SearchKit is working.

--- tests/SearchKitTest.php
+++ tests/SearchKitTest.php
@@ -27,32 +27,42 @@ class SearchKitTest extends BaseTestCase {
         if ( is_string( getenv('HUAWEI_APP_LEVEL_CLIENT_SECRET' ) ) ) {
             self::$oauth2_client_secret = getenv('HUAWEI_APP_LEVEL_CLIENT_SECRET');
         }
-        self::$client = new SearchKit( array_merge( self::get_user_config(), [
+        self::$client = new SearchKit( [
             'oauth2_client_id' => self::$oauth2_client_id,
-            'oauth2_client_secret' => self::$oauth2_client_secret,
-            'debug_mode' => false
-        ] ));
+            'oauth2_client_secret' => self::$oauth2_client_secret
+        ] );
         self::assertTrue( self::$client->is_ready(), self::CLIENT_NOT_READY );
     }
 
     /** Test: Web Search */
     public function test_web_search() {
         $result = self::$client->web_search( self::$web_search_term );
-        self::assertTrue($result->code == 200, $result->message );
+        self::assertTrue(property_exists($result, 'request_id') && is_string($result->request_id) );
+        self::assertTrue(property_exists($result, 'data') && is_array($result->data) );
+        echo print_r($result->data, true);
     }
 
+    /** Test: Image Search */
     public function test_image_search() {
         $result = self::$client->image_search( self::$image_search_term );
-        self::assertTrue($result->code == 200, $result->message );
+        self::assertTrue(property_exists($result, 'request_id') && is_string($result->request_id) );
+        self::assertTrue(property_exists($result, 'data') && is_array($result->data) );
+        echo print_r($result->data, true);
     }
 
+    /** Test: Video Search */
     public function test_video_search() {
         $result = self::$client->video_search( self::$video_search_term );
-        self::assertTrue($result->code == 200, $result->message );
+        self::assertTrue(property_exists($result, 'request_id') && is_string($result->request_id) );
+        self::assertTrue(property_exists($result, 'data') && is_array($result->data) );
+        echo print_r($result->data, true);
     }
 
+    /** Test: News Search */
     public function test_news_search() {
         $result = self::$client->news_search( self::$news_search_term );
-        self::assertTrue($result->code == 200, $result->message );
+        self::assertTrue(property_exists($result, 'request_id') && is_string($result->request_id) );
+        self::assertTrue(property_exists($result, 'data') && is_array($result->data) );
+        echo print_r($result->data, true);
     }
 }
